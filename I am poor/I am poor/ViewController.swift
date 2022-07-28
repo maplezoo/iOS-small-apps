b0d75f89e8150ffdfdbe46eec9274ebd1699ad88
Maple <maple.zhu7@gmail.com>
2022-07-28 08:45:35 -0700

    update the apps

---
+++ I am poor/I am poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am poor
+//
+//  Created by maple zhu on 7/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
